e9a5967252f4fd4d697cca7d8b5a1f121f644c17
OrgestPasha <orgestpasha2005@gmail.com>
2026-01-14 18:02:22 +0100

    Doing some haskell algo

--- haskellIO.hs
+++ haskellIO.hs
@@ -1,4 +1,10 @@
 main = do
-  putStrLn "hello what is your name"
-  name <- getLine
-  putStrLn ("Hey " ++ name ++ ", you rock")
+  line <- getLine
+  if null line
+    then return ()
+    else do
+      putStrLn $ reverseWords line
+      main
+
+reverseWords :: String -> String
+reverseWords = unwords . map reverse . words
